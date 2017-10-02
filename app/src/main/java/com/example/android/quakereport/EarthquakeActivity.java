c30f6831c95bdb71b4c0fa22ef716dc6afd96a75
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-10-02 10:42:21 +0530

    del placeholder city

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -39,7 +39,7 @@ public class EarthquakeActivity extends AppCompatActivity {
 //        earthquakes.add(new Earthquake ("2.8", "Mexico City", "May 4, 2017"));
 //        earthquakes.add(new Earthquake ("5.5", "Moscow", "Mar 17, 2017"));
 //        earthquakes.add(new Earthquake ("3.7", "Paris", "May 24, 2017"));
-//        earthquakes.add(new Earthquake ("1.8", "Calgary", "June 04, 2017"));
+
 
 
 
