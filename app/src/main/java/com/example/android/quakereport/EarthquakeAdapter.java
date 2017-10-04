4e206668d0cfe8aef138a24d91ef4952c4e710bf
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-10-04 10:48:16 +0530

    split the location string  into two different string in two different textView

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -16,6 +16,13 @@ import java.util.List;
  */
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    /**
+     * The part of the location string from the USGS service that we use to determine
+     * whether or not there is a location offset present ("5km N of Cairo, Egypt").
+     */
+    private static final String LOCATION_SEPARATOR = " of";
+
     /*
      *Construct a new {@Link EarthquakeAdapter}
      *
@@ -49,21 +56,35 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         // Display the magnitude of the current earthquake in that TextView
         magnitudeView.setText(currentEarthquake.getMagnitude());
 
-        // Find the TextView with view ID location
-        TextView locationViewOffset = (TextView) listItemView.findViewById(R.id.tv_location_offset);
-        // Display the location of the current earthquake in that TextView
-       // locationView.setText(currentEarthquake.getLocation());
-        String location = currentEarthquake.getLocation();
-        String[] locationSplit = location.split("of");
-        String offsetLocation = locationSplit[0];
-        String primaryLocation = locationSplit[1];
-        locationViewOffset.setText(offsetLocation);
+        // Get the original location string from the Earthquake object,
+        // which can be formatted of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
+        String originalLocation = currentEarthquake.getLocation();
 
+        // If the original location string (i.e. "5km N of Cairo, Egypt") contains
+        // a primary location (Cairo, Egypt) and a location offset (5 km N of city)
+        // then store the primary key location separately from the location offset in 2 strings,
+        // so they can be displayed in 2 TextViews.
+        String primaryLocation;
+        String offsetLocation;
 
-        // Find the TextView with view ID location
-        TextView locationViewPrimary = (TextView) listItemView.findViewById(R.id.tv_location_primary);
-        // Display the location of the current earthquake in that TextView
-        locationViewPrimary.setText(primaryLocation);
+        // Check whether the originalLocation string contains the "of" text\\
+        if(originalLocation.contains(LOCATION_SEPARATOR))   {
+            // Split the  originalLocation into two different parts (as an array of String)
+            // based on the "of" text. We except an array of 2 String, where
+            // the first String will be "5 km of" and second will be "Cairo, Egypt"
+            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
+            // Location offset should be  "5km N " + "of"
+            offsetLocation = parts[0] + LOCATION_SEPARATOR;
+            // Primary location should be "Cairo, Egypt"
+            primaryLocation = parts[1];
+        } else {
+            // Otherwise, there is no "of"  text in the originalLocation string
+            // Hence, set the default location offset to say "Near By"
+            offsetLocation = getContext().getString(R.string.near_the);
+            // primary location will be the full location string
+            primaryLocation = originalLocation;
+        }
+
 
 
         //Create a new Data object  from the time in milliseconds of the earthquake
