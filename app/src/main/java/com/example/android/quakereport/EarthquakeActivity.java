bb894b30b15b209ab72c11a33598541a50669200
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-10-20 21:02:00 +0530

    Remove placeholder city

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -41,7 +41,6 @@ public class EarthquakeActivity extends AppCompatActivity {
 //        earthquakes.add(new Earthquake ("5.3", "London","Aug 8, 2016"));
 //        earthquakes.add(new Earthquake ("7.6", "Tokyo", "Feb 14, 2017"));
 //        earthquakes.add(new Earthquake ("2.8", "Mexico City", "May 4, 2017"));
-//        earthquakes.add(new Earthquake ("5.5", "Moscow", "Mar 17, 2017"));
 
 
 
