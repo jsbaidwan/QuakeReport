fff5c97d3702cbcbc6989dde51104ce2d44672ac
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-12-06 16:38:46 +0530

    InputStreamReader handles the process of translation of  raw data into human readable character

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -12,9 +12,11 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 /**
@@ -89,6 +91,18 @@ public final class QueryUtils {
             }
         }
         return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream (InputStream inputStream) throws IOException  {
+        StringBuilder output =  new StringBuilder();
+        if(inputStream != null) {
+            InputStreamReader inputStreamReader =
+                    new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+        }
     }
 
     /**
