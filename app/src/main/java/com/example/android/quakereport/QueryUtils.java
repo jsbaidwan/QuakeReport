2b25fcac67c0dccbbb66f5523ee5a0c46a283cab
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-12-06 15:58:26 +0530

    HttpURLConnection object "GET" request to fetch data from api

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -11,6 +11,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -53,6 +55,16 @@ public final class QueryUtils {
         // If the URL is null, then return early.
         if (url == null)    {
             return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
         }
 
     }
