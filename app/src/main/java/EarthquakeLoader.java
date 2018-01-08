664e76896fcb44c268963e1e41548179d7ce90c6
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2018-01-08 15:43:03 +0530

    create EarrhquakeLoader class that extends AsyncTaskLoader

---
+++ app/src/main/java/EarthquakeLoader.java
@@ -0,0 +1,12 @@
+import android.content.AsyncTaskLoader;
+
+import com.example.android.quakereport.Earthquake;
+
+import java.util.List;
+
+/**
+ * Created by jaspreet.singh on 1/8/2018.
+ */
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+}
