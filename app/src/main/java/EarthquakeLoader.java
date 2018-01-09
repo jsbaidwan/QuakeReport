9c22945c869ab8186644798303f36b67b923caf3
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2018-01-09 22:13:17 +0530

    create EarrhquakeLoader class that extends AsyncTaskLoader

--- app/src/main/java/EarthquakeLoader.java
+++ app/src/main/java/EarthquakeLoader.java
@@ -8,5 +8,8 @@ import java.util.List;
  * Created by jaspreet.singh on 1/8/2018.
  */
 
+/** Loads a list of Earthquake by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
 public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 }
