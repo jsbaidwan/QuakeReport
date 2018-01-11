f4fe074ef4a5bd424df2ee7d683d19445e93d7cd
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2018-01-11 13:18:24 +0530

    create EarthquakeLoader class

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,11 @@
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import java.util.List;
+
+/**
+ * Created by jaspreet.singh on 1/11/2018.
+ */
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+}
