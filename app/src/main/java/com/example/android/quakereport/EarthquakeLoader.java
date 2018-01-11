78facdb59e9052cc4c008a0986a9c6912457e4b9
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2018-01-11 13:48:05 +0530

    create EarthquakeLoader constructor

--- app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 import android.content.AsyncTaskLoader;
+import android.content.Context;
+
 import java.util.List;
 
 /**
@@ -8,4 +10,9 @@ import java.util.List;
  */
 
 public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    public EarthquakeLoader (Context context, String url)   {
+        super(context);
+    }
+
 }
